83e54b33088cae0299381500e09429306012fc04
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-04-24 15:03:17 +0200

    adds queue_size to keyboard publisher

--- hand_control/src/teleop_twist_keyboard_azerty.py
+++ hand_control/src/teleop_twist_keyboard_azerty.py
@@ -63,7 +63,7 @@ def vels(speed,turn):
 
 if __name__=="__main__":
   settings = termios.tcgetattr(sys.stdin)
-  pub = rospy.Publisher('cmd_vel', Twist)
+  pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
   rospy.init_node('teleop_twist_keyboard_azerty')
   x = 0
   th = 0
