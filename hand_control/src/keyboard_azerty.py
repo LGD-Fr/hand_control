84e6b1a441d526a0e8035f2a804b19fe2f1cfb07
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-04-24 15:17:49 +0200

    change node name from keyboard_azerty.py

--- hand_control/src/keyboard_azerty.py
+++ hand_control/src/keyboard_azerty.py
@@ -64,7 +64,7 @@ def vels(speed,turn):
 if __name__=="__main__":
   settings = termios.tcgetattr(sys.stdin)
   pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-  rospy.init_node('teleop_twist_keyboard_azerty')
+  rospy.init_node('keyboard_azerty')
   x = 0
   th = 0
   y = 0
