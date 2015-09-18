c68b58ee7f8dd4a87074891ff9ed13f02270da0c
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-09-18 16:52:52 +0200

    adds GPL and FDL licenses

--- src/display.h
+++ src/display.h
@@ -1,7 +1,21 @@
-/* Copyright (C) 2015 CentraleSupélec
- * All rights reserved
+/* Copyright © 2015 CentraleSupélec
+ * 
+ * This file is part of Hand Control.
+ * 
+ * Hand Control is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Hand Control is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Hand Control.  If not, see <http://www.gnu.org/licenses/>.
  */
-// library to be used by "keyboard_cmd.cpp"
+
 #ifndef CURSES_DISPLAY
 #define CURSES_DISPLAY
 
